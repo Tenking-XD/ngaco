d50232868064a53cc91cda54004e10be08447fb0
Tenking-XD <109226204+Tenking-XD@users.noreply.github.com>
2022-07-14 01:32:44 +0700

    Delete run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-import os
-if __name__ == "__main__":
-   try:
-       os.system('git pull')
-       try:os.mkdir("Data")
-       except:pass
-       __import__("ige2").mengecek_()
-   except Exception as e:
-       exit(str(e))
